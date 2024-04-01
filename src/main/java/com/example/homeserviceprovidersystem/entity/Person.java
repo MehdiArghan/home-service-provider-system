cbc53368416a6160cb32e8f919f5ebcfed03fa83
mehdi <mehdiarghan@gmail.com.git/>
2024-04-01 20:18:04 +0330

    person entity was created

---
+++ src/main/java/com/example/homeserviceprovidersystem/entity/Person.java
@@ -0,0 +1,23 @@
+package com.example.homeserviceprovidersystem.entity;
+
+import com.example.homeserviceprovidersystem.base.BaseEntity;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@MappedSuperclass
+public class Person extends BaseEntity<Long> {
+    String firstName;
+    String lastName;
+    String email;
+    String password;
+    LocalDate RegistrationTime;
+}
