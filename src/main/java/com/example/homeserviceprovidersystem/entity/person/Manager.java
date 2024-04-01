fdacef90382de04df40b08832fd5920cb61a9a37
mehdi <mehdiarghan@gmail.com.git/>
2024-04-01 21:11:35 +0330

    Manager entity was created

---
+++ src/main/java/com/example/homeserviceprovidersystem/entity/person/Manager.java
@@ -0,0 +1,15 @@
+package com.example.homeserviceprovidersystem.entity.person;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class Manager extends Person {
+}
