fd08a3f593ee53db3bee18476a1a3d2f2c7cb92e
mehdi <mehdiarghan@gmail.com.git/>
2024-04-04 01:42:14 +0330

    created CustomRuntimeException

---
+++ src/main/java/com/example/homeserviceprovidersystem/customeException/CustomRuntimeException.java
@@ -0,0 +1,7 @@
+package com.example.homeserviceprovidersystem.customeException;
+
+public class CustomRuntimeException extends RuntimeException {
+    public CustomRuntimeException(String message) {
+        super(message);
+    }
+}
