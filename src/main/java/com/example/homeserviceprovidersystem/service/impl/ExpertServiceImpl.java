ba716e9cfc32a65020e7a8ab62b4586e6c993799
mehdi <mehdiarghan@gmail.com.git/>
2024-04-17 10:36:38 +0330

    created ExpertServiceImpl

---
+++ src/main/java/com/example/homeserviceprovidersystem/service/impl/ExpertServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.homeserviceprovidersystem.service.impl;
+
+import com.example.homeserviceprovidersystem.service.ExpertService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExpertServiceImpl implements ExpertService {
+}
