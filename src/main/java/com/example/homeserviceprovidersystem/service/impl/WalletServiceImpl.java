efd7c8526a2ecea85e2115322afbc8e3a868cbc9
mehdi <mehdiarghan@gmail.com.git/>
2024-04-19 17:58:27 +0330

    created WalletServiceImpl

---
+++ src/main/java/com/example/homeserviceprovidersystem/service/impl/WalletServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.homeserviceprovidersystem.service.impl;
+
+import com.example.homeserviceprovidersystem.entity.Wallet;
+import com.example.homeserviceprovidersystem.service.WalletService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WalletServiceImpl implements WalletService {
+    @Override
+    public Wallet save(Wallet wallet) {
+        return null;
+    }
+}
