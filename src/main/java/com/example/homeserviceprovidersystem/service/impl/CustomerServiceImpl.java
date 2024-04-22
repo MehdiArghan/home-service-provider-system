ea5477207ceab3f5567fb32b6143bb13301941ce
mehdi <mehdiarghan@gmail.com.git/>
2024-04-22 20:50:10 +0330

    created CustomerServiceImpl

---
+++ src/main/java/com/example/homeserviceprovidersystem/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.homeserviceprovidersystem.service.impl;
+
+import com.example.homeserviceprovidersystem.service.CustomerService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+}
