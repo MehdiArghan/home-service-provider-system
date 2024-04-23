01a470cadb61f217bebc4f2507ce6e02284a3e41
mehdi <mehdiarghan@gmail.com.git/>
2024-04-23 12:34:05 +0330

    added findAllSubDuty in CustomerController

--- src/main/java/com/example/homeserviceprovidersystem/controller/CustomerController.java
+++ src/main/java/com/example/homeserviceprovidersystem/controller/CustomerController.java
@@ -2,16 +2,18 @@ package com.example.homeserviceprovidersystem.controller;
 
 import com.example.homeserviceprovidersystem.dto.customerDto.CustomerDto;
 import com.example.homeserviceprovidersystem.dto.customerDto.CustomerDtoWithName;
+import com.example.homeserviceprovidersystem.dto.subDutyDto.SubDutyDto;
 import com.example.homeserviceprovidersystem.mapper.CustomerMapper;
+import com.example.homeserviceprovidersystem.mapper.SubDutyMapper;
 import com.example.homeserviceprovidersystem.service.CustomerService;
+import com.example.homeserviceprovidersystem.service.SubDutyService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,11 +21,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
     final CustomerMapper customerMapper;
     final CustomerService customerService;
+    final SubDutyMapper subDutyMapper;
+    final SubDutyService subDutyService;
 
     @PostMapping("/addCustomer")
     public ResponseEntity<CustomerDto> saveCustomer(@Valid @RequestBody CustomerDtoWithName customerDtoWithName) {
         CustomerDto savedCustomerDto = customerMapper.getCustomerToCustomerDto
                 (customerService.save(customerMapper.getCustomerDtoWithNameToCustomer(customerDtoWithName)));
         return new ResponseEntity<>(savedCustomerDto, HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/findAllSubDuty")
+    public ResponseEntity<List<SubDutyDto>> findAllSubDuty() {
+        List<SubDutyDto> subDutyDtoList = subDutyService.findAll()
+                .stream().map(subDutyMapper::getSubDutyToSubDutyDto).toList();
+        return new ResponseEntity<>(subDutyDtoList, HttpStatus.FOUND);
     }
 }
