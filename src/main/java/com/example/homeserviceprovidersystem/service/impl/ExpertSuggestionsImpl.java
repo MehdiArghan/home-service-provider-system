cce2a5f777cf650bfc7340630ca615367e753634
mehdi <mehdiarghan@gmail.com.git/>
2024-04-27 20:21:39 +0330

    created ExpertSuggestionsImpl

---
+++ src/main/java/com/example/homeserviceprovidersystem/service/impl/ExpertSuggestionsImpl.java
@@ -0,0 +1,8 @@
+package com.example.homeserviceprovidersystem.service.impl;
+
+import com.example.homeserviceprovidersystem.service.ExpertSuggestionsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExpertSuggestionsImpl implements ExpertSuggestionsService {
+}
